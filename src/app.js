ba53036db0a4c97c8827af74204da31929949a6a
FarzooqAsp <farzooqali333@gmail.com>
2024-06-15 15:18:58 +0500

    routes & postman configuration done

--- src/app.js
+++ src/app.js
@@ -10,4 +10,10 @@ app.use(express.json({limit:"15kb"}))
 app.use(express.urlencoded({extended:true,limit:"15kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+//import routers
+import router from "./routes/user.route.js"
+//routers declaration
+app.use("/api/v1/users", router)
+
 export {app}
--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -2,7 +2,7 @@ class ApiResponse {
     constructor(message="Success",data,StatusCode){
         this.message = message
         this.data = data
-        this.Status = StatusCode
+        this.status = StatusCode
         this.success = StatusCode < 400
     }
 }
